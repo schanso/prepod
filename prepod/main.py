64797c63d4a1cc6332f2f83ab10da07d39a95d2d
Jannes Schäfer <jott.ess@gmx.net>
2018-10-11 11:59:02 +0200

    Adds vanilla LDA

--- prepod/main.py
+++ prepod/main.py
@@ -1,5 +1,9 @@
+import numpy as np
+
 from prepod.lib.io import return_fnames, parse_raw, import_targets
-from prepod.lib.constants import COLNAME_SUBJID_SUDOCU, COLNAME_TARGET_SUDOCU
+from prepod.lib.constants import (COLNAME_SUBJID_SUDOCU, COLNAME_TARGET_SUDOCU,
+                                  EXCLUDE_SUBJ)
+from prepod.lib.models import train_test_wyrm, lda_vyrm
 from prepod.lib.prep import (align_bis, to_feature_vector, append_label,
                              merge_subjects)
 
@@ -19,7 +23,7 @@ path_out_merged = dir_out + 'merged.npy'
 
 fnames_raw = return_fnames(dir_in=dir_raw)
 subj_ids = sorted(list(set([el.split('_')[0] for el in fnames_raw])))
-
+subj_ids = [el for el in subj_ids if el not in EXCLUDE_SUBJ]
 
 # PARSE RAW FILES AND STORE AS NPY
 
@@ -30,8 +34,9 @@ for subj_id in subj_ids:
 
 # LOAD SUBJ DATA, APPEND LABELS, MERGE
 
+subj_ids_subset = ['2100', '2129', '2137', '2153', '2156', '2199']
 datasets = []
-for subj_id in subj_ids:
+for subj_id in subj_ids_subset:
     path_signal = dir_signal + return_fnames(dir_in=dir_signal, substr=subj_id)
     path_bis = dir_bis + subj_id + '/'
     data, bis = align_bis(path_signal=path_signal, path_bis=path_bis)
@@ -42,5 +47,13 @@ for subj_id in subj_ids:
     data = to_feature_vector(data)
     data = append_label(data, label)
     datasets.append(data)
-data = merge_subjects(datasets, path_out=path_out_merged)
+merge_subjects(datasets, path_out=path_out_merged)
+
+
+# CLASSIFICATION (VANILLA LDA)
+
+data = np.load(file=path_out_merged).flatten()[0]
+data_train, data_test = train_test_wyrm(data, test_size=.5)
+acc = lda_vyrm(data_train=data_train, data_test=data_test)
+print(acc)
 
